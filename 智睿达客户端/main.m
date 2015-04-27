0c6e36c378c2d34e41d19c5957feb7703231a8cb
tony1220 <tony1220@tony1220deMacBook-Air.local>
2015-04-27 10:04:08 +0800

    Initial Commit

---
+++ 智睿达客户端/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  智睿达客户端
+//
+//  Created by tony1220 on 15/4/27.
+//  Copyright (c) 2015年 tony1220. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
